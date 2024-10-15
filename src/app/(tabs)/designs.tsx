d3c0cdcd4e134075a1d01b22c4d589f7922e9e2b
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-10-15 16:45:31 +1100

    tabs in designs page

--- src/app/(tabs)/designs.tsx
+++ src/app/(tabs)/designs.tsx
@@ -1,11 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import Drafts from '../drafts'
+import Finished from '../finished'
 
+const Tab = createMaterialTopTabNavigator();
 const designs = () => {
   return (
-    <View>
-      <Text>designs</Text>
-    </View>
+    <Tab.Navigator screenOptions={{tabBarIndicatorStyle: {backgroundColor: "black"}}}>
+        <Tab.Screen name="Drafts" component={Drafts} />
+        <Tab.Screen name="Finished" component={Finished} />
+    </Tab.Navigator>
+    
   )
 }
 
---
+++ src/app/drafts.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Drafts = () => {
+  return (
+    <View>
+      <Text>drafts</Text>
+    </View>
+  )
+}
+
+export default Drafts
